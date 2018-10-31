33f30af15361347a7337fc3c0c48f292e348de93
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-10-31 20:44:49 -0300

    [WIP][#14][#15] migrating ladies view

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,29 @@
 from django.views.generic.base import TemplateView
+from django.views.generic.list import ListView
+
+from pathlib import Path
+import yaml
 
 
 class Home(TemplateView):
     template_name = "blog/index.html"
+
+
+class ListLadies(ListView):
+    template_name = 'blog/ladies.html'
+    context_object_name = 'ladies'
+
+    def get_context_data(self, **kwargs):
+        context = super(ListLadies, self).get_context_data()
+
+        context['medias'] = ['facebook', 'twitter', 'github']
+
+        return context
+
+    def get_queryset(self):
+        with open(f'{Path(__file__).parents[0]}/content/ladies.yml', 'r') as stream:
+            try:
+                ladies = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                ladies = []
+        return ladies
