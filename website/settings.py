abaca285a2b1445698c9931d80642aa51056498b
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-11-01 01:36:51 -0300

    [#16] add allowed hosts

--- website/settings.py
+++ website/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '1)v8&@!#+sr^n3#ppifle&=m*@%t%h77)33whql=pneew+s_)6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['df.pyladies.com', 'pyladiesdf.herokuapp.com']
 
 
 # Application definition
