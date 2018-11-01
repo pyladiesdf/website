313ba90ec9911be17e002d982c2126710262e37e
Dayanne Fernandes <dayannefernandesc@gmail.com>
2018-11-01 01:28:52 -0300

    [#16] add gunicorn and django heroku

--- website/settings.py
+++ website/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
+import django_heroku
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,3 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
