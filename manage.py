348e2fb6ea0903f35f818e082f4a48a7717cff6a
Dayanne Fernandes <dayannefernandesc@gmail.com>
2020-10-14 11:26:03 -0300

    bump some lib versions and fix db url for local test

--- manage.py
+++ manage.py
@@ -1,8 +1,15 @@
-#!/usr/bin/env python
 import os
 import sys
 
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+
 if __name__ == '__main__':
+    debug = os.getenv('DEBUG', True)
+    if debug:  # local
+        os.environ.setdefault('DATABASE_URL',
+                              'sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3')))
+
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'website.settings')
     try:
         from django.core.management import execute_from_command_line
